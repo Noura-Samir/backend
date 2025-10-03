23951966523f6aaf966197fa3b4b60e8d76d0665
Noura-Samir <samirnoura857@gmail.com>
2025-10-03 20:43:54 +0300

    solve error

--- backend/api/app.js
+++ backend/api/app.js
@@ -31,7 +31,7 @@ app.use(cors({
 app.options("*", cors({
   origin: [
     "http://localhost:4200",
-    "https://frontend-6t9tqphj6-noura-samirs-projects.vercel.app"
+    "https://frontend-noura-samirs-projects.vercel.app/"
   ],
   credentials: true
 }));
